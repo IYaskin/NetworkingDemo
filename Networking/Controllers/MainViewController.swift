de718f56cc07a6ba3d9cafb17f29e29f32b6753d
Igor <igoryaskinwork@gmail.com>
2019-10-17 13:39:14 +0300

    Added "Download data"

--- Networking/Controllers/MainViewController.swift
+++ Networking/Controllers/MainViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import UserNotifications
 
 enum Actions: String, CaseIterable {
     
     case downloadImage = "Download Image"
     case get = "GET"
     case post = "POST"
-    case ourCourses = "Our courses"
+    case ourCourses = "Our Courses"
     case uploadImage = "Upload Image"
+    case downloadFile = "Download File"
 }
 
 private let reuseIdentifier = "Cell"
@@ -24,7 +26,57 @@ private let uploadImage = "https://api.imageban.ru/v1"
 class MainViewController: UICollectionViewController {
     
     let actions = Actions.allCases
+    private var alert: UIAlertController!
+    private let dataProvider = DataProvider()
+    private var filePath: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        registerForNotification()
 
+        dataProvider.fileLocation = { (location) in
+            //Save file for example
+            print("Download finished: \(location.absoluteString)")
+            self.filePath = location.absoluteString
+            self.alert.dismiss(animated: false, completion: nil)
+            self.postNotification()
+        }
+        
+    }
+    
+    private func showAlert() {
+        alert = UIAlertController(title: "Downloading...", message: "0%", preferredStyle: .alert)
+        
+        let heightOfAlert = NSLayoutConstraint(item: alert.view!, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: 150)
+        
+        alert.view.addConstraint(heightOfAlert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { (action) in
+            self.dataProvider.stopDownload()
+            
+        }
+        alert.addAction(cancelAction)
+        present(alert, animated: true) {
+            
+            let size = CGSize(width: 40, height: 40)
+            let point = CGPoint(x: (self.alert.view.frame.width / 2) - (size.width / 2) , y: (self.alert.view.frame.height / 2) - (size.height / 2))
+            
+            let activityIndicator = UIActivityIndicatorView(frame: CGRect(origin: point, size: size))
+            activityIndicator.color = .gray
+            activityIndicator.startAnimating()
+            
+            let progressView = UIProgressView(frame: CGRect(x: 0, y: self.alert.view.frame.height - 44, width: self.alert.view.frame.width, height: 2))
+            progressView.tintColor = .blue
+
+            self.dataProvider.onProgress = { (progress) in
+                progressView.progress = Float(progress)
+                self.alert.message = String(Int(progress * 100)) + "%"
+            }
+            
+            self.alert.view.addSubview(activityIndicator)
+            self.alert.view.addSubview(progressView)
+        }
+    }
 
     // MARK: UICollectionViewDataSource
 
@@ -56,7 +108,10 @@ class MainViewController: UICollectionViewController {
         case .ourCourses:
             performSegue(withIdentifier: "OurCourses", sender: self)
         case .uploadImage:
-            NetworkManager.uploadImageOld(url: uploadImage)
+            NetworkManager.uploadImage(url: uploadImage)
+        case .downloadFile:
+            showAlert()
+            dataProvider.startDownload()
     
         }
     }
@@ -91,4 +146,26 @@ class MainViewController: UICollectionViewController {
     }
     */
 
+}
+
+extension MainViewController {
+    
+    private func registerForNotification() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (_, _) in
+            
+        }
+    }
+    
+    private func postNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Download Complete!"
+        content.body = "Your background transfer has completed. File path: \(filePath!)"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "TransferComplete", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
 }
---
+++ Networking/Network Layer/DataProvider.swift
@@ -0,0 +1,83 @@
+//
+//  DataProvider.swift
+//  Networking
+//
+//  Created by Igor Yaskin on 17/10/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class DataProvider: NSObject {
+    
+    private var downloadTask: URLSessionDownloadTask!
+    var fileLocation: ((URL) -> ())?
+    var onProgress: ((Double) -> ())?
+    
+    private lazy var bgSession: URLSession = {
+        let config = URLSessionConfiguration.background(withIdentifier: "ru.yaskin.Networking")
+        //Networking3
+//        config.isDiscretionary = true
+        config.sessionSendsLaunchEvents = true
+        return URLSession(configuration: config, delegate: self, delegateQueue: nil)
+    }()
+
+    func startDownload() {
+        
+        if let url = URL(string: "https://speed.hetzner.de/100MB.bin") {
+            downloadTask = bgSession.downloadTask(with: url)
+            downloadTask.earliestBeginDate = Date().addingTimeInterval(3)
+            downloadTask.countOfBytesClientExpectsToSend = 512
+            downloadTask.countOfBytesClientExpectsToReceive = 100 * 1024 * 1024
+            downloadTask.resume()
+        }
+        
+    }
+    
+    func stopDownload() {
+        downloadTask.cancel()
+    }
+
+}
+
+extension DataProvider: URLSessionDelegate {
+    
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        
+        DispatchQueue.main.async {
+            guard
+                let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+                let completionHandler = appDelegate.bgSessionCompletionHandler
+                else { return }
+            
+            appDelegate.bgSessionCompletionHandler = nil
+            completionHandler()
+        }
+        
+    }
+    
+}
+
+extension DataProvider: URLSessionDownloadDelegate {
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("Did finishwnloading: \(location.absoluteString)")
+        
+        DispatchQueue.main.async {
+            self.fileLocation?(location)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        guard totalBytesExpectedToWrite != NSURLSessionTransferSizeUnknown else { return }
+        
+        let progress = Double(totalBytesWritten) / Double(totalBytesExpectedToWrite)
+        print("Download progress: \(progress * 100)%")
+        
+        DispatchQueue.main.async {
+            self.onProgress?(progress)
+        }
+    }
+    
+}
